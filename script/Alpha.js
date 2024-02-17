f2fb3537392effb7c2ed4a937a30c90ba23501ef
Farid <mdfaridhossenrehad@gmail.com>
2024-02-17 21:38:02 +0600

    Hidden and Show process added. Function and Normal process both are shown

--- script/Alpha.js
+++ script/Alpha.js
@@ -1,3 +1,18 @@
+// function play(){
+//     // step:1 hide the home screen. To the screen add the class hidden to
+//     // the home section
+//     const homeSection = document.getElementById('home-screen');
+//     homeSection.classList.add('hidden');
+//     // console.log(homeSection.classList)
+    
+
+//     // show the playground
+//     const playGround = document.getElementById('play-ground');
+//     playGround.classList.remove('hidden');
+// }
+
+
 function play(){
-    console.log('here');
+    hideElementById('home-screen');
+    showElementById('play-ground');
 }
