5eb956040623adbd25eb156f22aca8b76d52094a
Farid <mdfaridhossenrehad@gmail.com>
2024-02-18 22:14:13 +0600

    Score board updated

--- script/Alpha.js
+++ script/Alpha.js
@@ -12,7 +12,76 @@
 // }
 
 
+
+function handleKeyboardButtonPress(event){
+    const playerPressed = event.key;
+    // console.log('button pressed');
+    // console.log(playerPressed);
+    
+    // get the expected to press
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabet = currentAlphabet.toLowerCase();
+    // console.log(playerPressed,expectedAlphabet);
+
+    if(playerPressed === expectedAlphabet){
+        console.log('Right');
+        // console.log('you have pressed correctly',expectedAlphabet);
+        //update score
+        //1. get the current score
+
+        const currentScoreElement = document.getElementById('current-score');
+        const currentScoreText = currentScoreElement.innerText;
+        const currentScore = parseInt(currentScoreText);
+        // console.log(currentScore);
+
+        //2. increase the score by 1
+        const newScore = currentScore+1;
+
+        //3. show the updated score
+        currentScoreElement.innerText = newScore;
+
+
+        //start a new round
+        removeBackgroundColorById(expectedAlphabet);
+        continueGame();
+    }
+    else{
+        // console.log('Wrong');
+        //step-1: get the current life number
+        const currentLifeElement = document.getElementById('current-life');
+        const currentLifeText = currentLifeElement.innerText;
+        const currentLife = parseInt(currentLifeText);
+
+
+        //step-2:reduce the life count
+        const newLife = currentLife-1;
+        console.log(newLife,currentLife);
+
+        //step-3:display the updated life count
+        currentAlphabetElement.innerText = newLife;
+        
+    }
+}
+document.addEventListener('keyup',handleKeyboardButtonPress);
+
+function continueGame(){
+    // step-1: Generate a random alphabet
+    const alphabet = getRandomAlphabet();
+    // console.log('your alphabet',alphabet)
+
+    //set randomly generated alphabet to the screen (show it)
+    // console.log('your random alphabet ',alphabet);
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    currentAlphabetElement.innerText = alphabet;
+
+    //set background color 
+    setBackgroundById(alphabet);
+
+}
+
 function play(){
     hideElementById('home-screen');
     showElementById('play-ground');
+    continueGame();
 }
--- script/uitily.js
+++ script/uitily.js
@@ -7,3 +7,31 @@ function showElementById(elementId){
     const element = document.getElementById(elementId);
     element.classList.remove('hidden');
 }
+
+function setBackgroundById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.add('bg-orange-400');
+}
+function removeBackgroundColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-orange-400')
+}
+
+function getRandomAlphabet(){
+    // get or create an alphabet array
+    const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
+    const alphabets = alphabetString.split('');
+    // console.log(alphabets);
+
+    // get a random index between 0-25
+    const randomNumber = Math.random()*25;
+    const index = Math.round(randomNumber);
+    // console.log(index);
+
+    // const alphabet = alphabets;
+    // return alphabet;
+
+    const alphabet = alphabets[index];
+    
+    return alphabet;
+}
